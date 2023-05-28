ffa09768734498b2500ea3bb6de18d7d1a9b297b
smerch88 <maxarmyk@gmail.com>
2023-05-28 21:07:32 +0300

    feat: add some test and readme

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { CreateSuperheroDto } from 'src/superheroes/dto/create-superhero.dto';
+import { Superhero } from 'src/superheroes/superhero.entity';
+import { SuperheroesController } from 'src/superheroes/superheroes.controller';
+import { SuperheroesService } from 'src/superheroes/superheroes.service';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('SuperheroesController', () => {
+  let superheroesController: SuperheroesController;
+  let superheroesService: SuperheroesService;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [SuperheroesController],
+      providers: [SuperheroesService],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    superheroesController = module.get<SuperheroesController>(
+      SuperheroesController,
+    );
+    superheroesService = module.get<SuperheroesService>(SuperheroesService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('getSuperheroById', () => {
+    it('should return a superhero with the given ID', async () => {
+      const mockId = 1;
+      const mockSuperhero = new Superhero();
+      jest
+        .spyOn(superheroesService, 'getSuperheroById')
+        .mockResolvedValue(mockSuperhero);
+
+      const result = await superheroesController.getSuperheroById(mockId);
+
+      expect(result).toEqual(mockSuperhero);
+      expect(superheroesService.getSuperheroById).toHaveBeenCalledWith(mockId);
+    });
+  });
+
+  describe('createSuperhero', () => {
+    it('should create and return a new superhero', async () => {
+      const mockCreateDto: CreateSuperheroDto = {
+        nickname: 'Superman',
+        real_name: 'Clark Kent',
+        origin_description: 'The Last Son of Krypton',
+        superpowers: 'Flight, Super Strength, Heat Vision',
+        catch_phrase: 'Up, up, and away!',
+        imageLinks: [],
+      };
+      const mockSuperhero = new Superhero();
+      jest
+        .spyOn(superheroesService, 'createSuperhero')
+        .mockResolvedValue(mockSuperhero);
+
+      const result = await superheroesController.createSuperhero(mockCreateDto);
+
+      expect(result).toEqual(mockSuperhero);
+      expect(superheroesService.createSuperhero).toHaveBeenCalledWith(
+        mockCreateDto,
+      );
+    });
   });
 });
